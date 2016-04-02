787884579563450e8ba8e7e885e5b62d76c0a5b3
num3ric <eric.renaud.houde@gmail.com>
2016-04-02 16:52:59 -0400

    First commit of gui demo.

---
+++ samples/DemoGUI/src/DemoGUIApp.cpp
@@ -0,0 +1,80 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include "cinder/params/Params.h"
+#include "cinder/Perlin.h"
+
+#include "Var.h"
+
+using namespace ci;
+using namespace ci::app;
+
+struct Disk {
+	Disk( const std::string& varGroup )
+	: mRadius{ 0.0f, "radius", varGroup, []() { app::console() << "Updated disk radius!" << std::endl; } }
+	, mColor{ Color{}, "color", "disk" }
+	, mPos{ app::getWindowSize() / 2 }
+	{ }
+	
+	Var<float>	mRadius;
+	Var<Color>	mColor;
+	glm::vec2	mPos, mVel;
+};
+
+class DemoGUIApp : public App {
+public:
+	DemoGUIApp();
+	void update() override;
+	void draw() override;
+	void keyDown( KeyEvent event ) override;
+	
+	Disk					mDisk;
+	Perlin					mPerlin;
+	Var<float>				mPerlinAmplitude, mPerlinSpeed, mPerlinScale, mFriction, mSpringK;
+};
+
+DemoGUIApp::DemoGUIApp()
+: mDisk{ "disk" }
+, mPerlinScale( 0.001f, "scale", "perlin" )
+, mPerlinAmplitude( 0.5f, "amplitude", "perlin" )
+, mPerlinSpeed( 1.0f, "speed", "perlin" )
+, mFriction( 0.949999988f, "friction" )
+, mSpringK( 0.0025f, "springk" )
+{
+	//	bag()->load(); //called by watchdog automatically
+}
+
+void DemoGUIApp::update()
+{
+	float time = app::getElapsedSeconds();
+	auto acc = mPerlinAmplitude() * vec2( mPerlin.dfBm( vec3( mPerlinScale() * vec2( mDisk.mPos.x, mDisk.mPos.y ), mPerlinSpeed * time ) ) );
+	mDisk.mVel += acc + mSpringK() * ( vec2( app::getWindowSize() / 2 ) - mDisk.mPos );
+	mDisk.mVel *= mFriction();
+	mDisk.mPos += mDisk.mVel;
+}
+
+void DemoGUIApp::draw()
+{
+	gl::clear( Color::gray( 0.5f ) );
+	
+	gl::ScopedColor col;
+	gl::color( mDisk.mColor );
+	gl::drawSolidCircle( mDisk.mPos, mDisk.mRadius );
+}
+
+void DemoGUIApp::keyDown( KeyEvent event )
+{
+	if( event.getCode() == KeyEvent::KEY_s ) {
+		bag()->save();
+	}
+	else if( event.getCode() == KeyEvent::KEY_l ) {
+		bag()->load();
+	}
+	else if( event.getCode() == KeyEvent::KEY_r ) {
+		mDisk.mPos = vec2( app::getWindowSize() / 2 );
+	}
+}
+
+CINDER_APP( DemoGUIApp, RendererGl )
+
